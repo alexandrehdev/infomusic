3579eb0a04c2404bc9b5a0406430d514974e450c
alexandrehdev <hallyssonrdev@gmail.com>
2022-08-19 20:57:27 -0300

    send request working

--- src/Controller/Service.php
+++ src/Controller/Service.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Music\Controller;
+use App\Music\Controller\Builder;
 use App\Music\Utils\View;
 
 class Service{
@@ -7,5 +8,13 @@ class Service{
     public static function callHome(){
        echo View::getView("principal");        
     } 
+
+    public static function testingSearch(){
+        $builder = new Builder;
+        $search = $builder->getSearch();
+        echo "
+            Você procurou por <strong>{$search} </strong> :)
+         ";
+    }
 
 }
